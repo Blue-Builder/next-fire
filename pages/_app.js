d48d74f05a8144552856d94b5fa621c28087f348
Etuk Mfon <emfon71@gmail.com>
2021-03-12 18:11:17 +0100

    added toast message support

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import Navbar from '@components/Navbar.js'
 import '@styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
       <Navbar />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
+      <Toaster />
     </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Loader from '@components/Loader.js'
+import { toast } from 'react-hot-toast'
 
 export default function IndexPage() {
   return (
     <main>
       <div>IndexPage page</div>
+      <button type='button' onClick={() => toast.success('hello toast!')}>
+        Toast Me
+      </button>
       <Loader show />
     </main>
   )
